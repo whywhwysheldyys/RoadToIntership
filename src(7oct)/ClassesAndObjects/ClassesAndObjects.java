a22140193913d5c011b338ac1fa36e5cd212eb49
whywhwysheldyys <basalajandrej95@gmail.com>
2025-10-07 16:19:08 +0300

    Add files via upload

---
+++ src(7oct)/ClassesAndObjects/ClassesAndObjects.java
@@ -0,0 +1,119 @@
+package ClassesAndObjects;
+
+//Практическая задача 1
+//
+//Создать класс Car с полями:
+//
+//brand (строка)
+//
+//year (число)
+//
+//Создать два объекта Car, присвоить им значения, и вывести их на экран.
+//class Car {
+//    String brand;
+//    int year;
+//
+//class Book {
+//    String title = "Unknown"; // значение по умолчанию
+//    int pages = 100;          // значение по умолчанию
+//}
+
+//class Student {
+//    String name;
+//    int grade;
+//}
+//class Rectangle {
+//    double length;
+//    double width;
+//}
+public class ClassesAndObjects {
+    public static void main(String[] args) {
+//        Car car1 = new Car();
+//        car1.brand = "Toyota";
+//        car1.year = 2020;
+//
+//
+//        Car car2 = new Car();
+//        car2.brand = "Honda";
+//        car2.year = 2022;
+//
+//        System.out.println("Car 1: " + car1.brand + ", " + car1.year);
+//        System.out.println("Car 2: " + car2.brand + ", " + car2.year);
+
+
+//        Практическая задача 2
+//
+//        Создать класс Book с полями:
+//
+//        title (строка)
+//
+//        pages (число)
+//
+//        Задать полям значения по умолчанию:
+//
+//        title = "Unknown"
+//
+//        pages = 100
+//
+//        Создать объект Book и вывести его поля на экран.
+
+//        Book myBook = new Book();
+//
+//        System.out.println("Title: " + myBook.title);
+//        System.out.println("Pages: " + myBook.pages);
+
+
+//
+
+
+//        Практическая задача 3
+//
+//        Создать класс Student с полями:
+//
+//        name (строка)
+//
+//        grade (число)
+//
+//        Создать три объекта Student, присвоить им разные значения и вывести их на экран.
+
+
+
+//        Student student1 = new Student();
+//        student1.name = "Alice";
+//        student1.grade = 90;
+//
+//
+//        Student student2 = new Student();
+//        student2.name = "Bob";
+//        student2.grade = 85;
+//
+//
+//        Student student3 = new Student();
+//        student3.name = "Charlie";
+//        student3.grade = 92;
+//
+//        System.out.println(student1.name + " - " + student1.grade);
+//        System.out.println(student2.name + " - " + student2.grade);
+//        System.out.println(student3.name + " - " + student3.grade);
+
+//        Практическая задача 4
+//
+//        Создать класс Rectangle с полями:
+//
+//        length (число)
+//
+//        width (число)
+//
+//        Создать объект rect, присвоить длину и ширину, и вывести площадь прямоугольника на экран.
+
+//        Rectangle rect = new Rectangle();
+//        rect.length = 5.0;
+//        rect.width = 3.0;
+//
+//        double area = rect.length * rect.width;
+//
+//        System.out.println("Площадь прямоугольника: " + area);
+            }
+        }
+
+
